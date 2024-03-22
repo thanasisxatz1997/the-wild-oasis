8a6dce6656be8ddda4e244153746d58ac10c2450
thanasisxatz1997 <93034841+thanasisxatz1997@users.noreply.github.com>
2024-03-22 15:08:35 +0200

    ReactQuery

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,23 @@ import Login from "./pages/Login";
 import PageNotFount from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient } from "@tanstack/react-query";
+// import { QueryClientProvider } from "@tanstack/react-query/build/lib/QueryClientProvider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,7 +43,7 @@ function App() {
           <Route path="*" element={<PageNotFount />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/ui/Spinner.jsx
+++ src/ui/Spinner.jsx
@@ -8,7 +8,6 @@ const rotate = keyframes`
 
 const Spinner = styled.div`
   margin: 4.8rem auto;
-
   width: 6.4rem;
   aspect-ratio: 1;
   border-radius: 50%;
