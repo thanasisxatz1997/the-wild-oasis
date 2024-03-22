57f211828f24b2cf0850168b9076a9405e34d4e1
thanasisxatz1997 <93034841+thanasisxatz1997@users.noreply.github.com>
2024-03-22 17:48:06 +0200

    deleting-cabins

--- src/data/services/apiCabins.js
+++ src/data/services/apiCabins.js
@@ -3,8 +3,18 @@ import supabase from "./supabase";
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
   if (error) {
-    console.log("Cabins could not be loaded.");
+    console.log(error);
     throw new Error("Cabins could not be loaded.");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.log(error);
+    throw new Error("Cabins could not be deleted.");
   }
 
   return data;
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,6 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import styled from "styled-components";
+import { deleteCabin } from "../../data/services/apiCabins";
 import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
@@ -41,7 +43,29 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
+
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: (id) => deleteCabin(id),
+    //same as mutationFn: deleteCabin because the parameters are the same
+    onSuccess: () => {
+      alert("Cabin successfully deleted");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (err) => alert(err.message),
+  });
+
   return (
     <TableRow role="row">
       <Img src={image} />
@@ -49,7 +73,9 @@ function CabinRow({ cabin }) {
       <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button>Delete</button>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+        Delete
+      </button>
     </TableRow>
   );
 }
